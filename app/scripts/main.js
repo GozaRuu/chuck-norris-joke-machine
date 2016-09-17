4313cf2b3ab90b36bcd5dc78ed17177f6cacad02
GozaRuu <kais.sghari@gmail.com>
2016-09-17 18:55:05 +0100

    UI bugs fixed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,14 @@
-var colors = ['#ee1c27','#2b003a','#08391b']
+var colors = ['#ee1c27','#2b003a','#00887c','#8c0049']
 var clicks=0 ;
 
 $('.more-content-button').on('click',function(){
+  if(clicks===6000){
+    clicks = -1 ;
+  }
   clicks++ ;
-  var color = colors[clicks%3] ;
-  $('.color-shifter').css('color',color) ;
+  var color = colors[clicks%4] ;
+  $('.text').css('color',color) ;
+  $('.title').css('color',color) ;
+  $('.nav-button').css('color',color) ;
   $('.background-color-shifter').css('background',color) ;
 }) ;
