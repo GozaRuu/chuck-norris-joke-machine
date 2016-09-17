7542fb4fa1da5ee29670a343c72057fc2a233a70
GozaRuu <kais.sghari@gmail.com>
2016-09-17 19:22:36 +0100

    connection to the API and AJAX added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,10 +2,25 @@ var colors = ['#ee1c27','#2b003a','#00887c','#8c0049']
 var clicks=0 ;
 
 $('.more-content-button').on('click',function(){
+  //house-keeping
   if(clicks===6000){
     clicks = -1 ;
   }
   clicks++ ;
+
+  //ajax content bringing
+
+  $.ajax({
+  url: "https://api.chucknorris.io/jokes/random"
+})
+  .done(function( data ) {
+    // if ( console && console.log ) {
+    //   console.log( "Sample of data:", data.value);
+    // }
+    $('.text').text(data.value) ;
+  });
+
+  //color changing
   var color = colors[clicks%4] ;
   $('.text').css('color',color) ;
   $('.title').css('color',color) ;
