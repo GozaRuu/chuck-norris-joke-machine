0561bba74c7f118bc3ebc1e677f7661b7e671d9c
GozaRuu <kais.sghari@gmail.com>
2016-09-23 23:33:38 +0100

    UI enhanced, added some js fonctionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,16 +6,29 @@ $.ready(function() {
     console.log( "ready!" );
 });
 
-$.ready(function() {
-  $.ajax({
-  url: "https://api.chucknorris.io/jokes/random"
-}).done(function( data ) {
-    $('.text').text(data.value) ;
-  }).fail(fuction(){
-    $('.text').text('error sorry :(') ;
-  });
+$('#about-btn').click(function(){
+  $('#about').removeClass('hidden') ;
+  $('#contact').addClass('hidden') ;
+});
 
-}) ;
+$('#contact-btn').click(function(){
+  $('#contact').removeClass('hidden') ;
+  $('#about').addClass('hidden') ;
+});
+
+$('.modal').on('blur', function(){
+  $('#contact').addClass('hidden') ;
+  $('#about').addClass('hidden') ;
+});
+
+$('.close-about').click(function(){
+  $('#about').toggleClass('hidden') ;
+});
+
+$('.close-contact').click(function(){
+  $('#contact').toggleClass('hidden') ;
+});
+
 
 $('.more-content-button').on('click',function(){
   //house-keeping
@@ -33,10 +46,10 @@ $('.more-content-button').on('click',function(){
     // if ( console && console.log ) {
     //   console.log( "Sample of data:", data.value);
     // }
-    $('.text').text(data.value) ;
-  }).fail(fuction(){
-    $('.text').text('error sorry :(') ;
-  };
+    $('.mid .text').text(data.value) ;
+  }).fail(function(){
+    $('.mid .text').text('error sorry :(') ;
+  });
 
   //color changing
   var color = colors[clicks%(colors.length)] ;
@@ -45,8 +58,3 @@ $('.more-content-button').on('click',function(){
   $('.nav-button').css('color',color) ;
   $('.background-color-shifter').css('background',color) ;
 }) ;
-
-
-$('#about-btn').on('click',function(){
-  $('#about').removeClass('hidden')
-});
