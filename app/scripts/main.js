9725593e3ee7d820002a5d7dba261032b89ffd25
GozaRuu <kais.sghari@gmail.com>
2016-09-23 21:18:12 +0100

    fixed chrome problems and added modal styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,21 @@
-var colors = ['#ee1c27','#2b003a','#00887c','#8c0049']
+var colors = ['#ee1c27','#2b003a','#00887c','#8c0049','#27ae60','#f39c12', '#FB6964',"#472E32", "#77326E"] ;
 var clicks=0 ;
+
+
+$.ready(function() {
+    console.log( "ready!" );
+});
+
+$.ready(function() {
+  $.ajax({
+  url: "https://api.chucknorris.io/jokes/random"
+}).done(function( data ) {
+    $('.text').text(data.value) ;
+  }).fail(fuction(){
+    $('.text').text('error sorry :(') ;
+  });
+
+}) ;
 
 $('.more-content-button').on('click',function(){
   //house-keeping
@@ -18,12 +34,19 @@ $('.more-content-button').on('click',function(){
     //   console.log( "Sample of data:", data.value);
     // }
     $('.text').text(data.value) ;
-  });
+  }).fail(fuction(){
+    $('.text').text('error sorry :(') ;
+  };
 
   //color changing
-  var color = colors[clicks%4] ;
+  var color = colors[clicks%(colors.length)] ;
   $('.text').css('color',color) ;
   $('.title').css('color',color) ;
   $('.nav-button').css('color',color) ;
   $('.background-color-shifter').css('background',color) ;
 }) ;
+
+
+$('#about-btn').on('click',function(){
+  $('#about').removeClass('hidden')
+});
